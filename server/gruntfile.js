835885ed8d2288a213bcafb9505863bda8cf96ad
davidlukerice <davidlukerice@gmail.com>
2015-01-29 20:41:35 -0600

    basic socket.io server

---
+++ server/gruntfile.js
@@ -0,0 +1,153 @@
+
+var config = {
+  app: 'public',
+  dist: 'dist'
+};
+
+module.exports = function(grunt) {
+  // Project Configuration
+  grunt.initConfig({
+    config: config,
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: [
+        'gruntfile.js',
+        'server/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['server/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+        }
+      },
+    },
+    nodemon: {
+      dev: {
+        script: 'server.js',
+        options: {
+          env: {
+            NODE_ENV: 'development'
+          },
+          nodeArgs: ['--debug'],
+          ignore: ['node_modules/**']
+        }
+      },
+      app: {
+        script: 'app-server.js',
+        options: {
+          env: {
+            NODE_ENV: 'development'
+          },
+          nodeArgs: [],
+          ignore: ['node_modules/**']
+        }
+      },
+      prod: {
+        script: 'server.js',
+        options: {
+          env: {
+            NODE_ENV: 'production'
+          },
+          watch: []
+        }
+      },
+      'app-prod': {
+        script: 'app-server.js',
+        options: {
+          env: {
+            NODE_ENV: 'production'
+          },
+          watch: []
+        }
+      }
+    },
+    concurrent: {
+      dev: {
+        tasks: ['nodemon:dev', 'node-inspector', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      },
+      'dev-full': {
+        tasks: ['nodemon:dev', 'nodemon:app', 'node-inspector', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      },
+      prod: {
+        tasks: ['nodemon:prod', 'nodemon:app-prod'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+
+    },
+    'node-inspector': {
+      custom: {
+        options: {
+          'web-port': 1337,
+          'web-host': 'localhost',
+          'debug-port': 5858,
+          'save-live-edit': true,
+          'no-preload': true,
+          'stack-trace-limit': 50,
+          'hidden': []
+        }
+      }
+    },
+    env: {
+      test: {
+        NODE_ENV: 'test'
+      }
+    },
+    mochaTest: {
+      src: ['test/**/*.js'],
+      options: {
+        reporter: 'spec',
+        require: 'server.js'
+      }
+    },
+    clean: {
+      server: '.tmp'
+    }
+  });
+
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-node-inspector');
+
+  grunt.option('force', true);
+
+  //Default task(s).
+  grunt.registerTask('default', [
+    'jshint',
+    //'clean:server',
+    'concurrent:dev'
+  ]);
+
+  grunt.registerTask('devfull', [
+    'jshint',
+    //'clean:server',
+    'concurrent:dev-full'
+  ]);
+
+  grunt.registerTask('prod', [
+    //'clean:server',
+    'concurrent:prod'
+  ]);
+
+  //Test task.
+  grunt.registerTask('test', ['env:test', 'mochaTest']);
+};
