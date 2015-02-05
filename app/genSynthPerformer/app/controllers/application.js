90de1887fb369565100b9806749100e5d2b3e851
davidlukerice <davidlukerice@gmail.com>
2015-02-05 00:15:04 -0600

    access socket from other controllers

--- app/genSynthPerformer/app/controllers/application.js
+++ app/genSynthPerformer/app/controllers/application.js
@@ -4,18 +4,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  socket: null,
+
   initializeSocket: function() {
     var socket = io.connect('http://localhost:3000');
-    this.socket = socket;
+    this.set('socket', socket);
     //socket.on('chat message', function(msg){
     //  $('#messages').append($('<li>').text(msg));
     //});
   }.on('init'),
 
   actions: {
-    updateInstruments: function() {
-      this.socket.emit('chat message', $('#m').val());
-      $('#m').val('');
-    }
   }
 });
